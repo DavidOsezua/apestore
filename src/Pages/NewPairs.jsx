67250a72f2912e55613c06e3cf215f0b918ccc2b
David Osezua <davidosez78@gmail.com>
2024-06-06 09:51:24 +0100

    styled the first set of cards

--- src/Pages/NewPairs.jsx
+++ src/Pages/NewPairs.jsx
@@ -5,12 +5,14 @@ const NewPairs = () => {
   return (
     <section className='p-7'>
  <div>
-  <div className='flex justify-items-center'>
+  <div className='flex justify-center gap-5'>
+  <Card/>
+  <Card/>
   <Card/>
   </div>
 
 
-<h3 className='py-[2rem]'>Real time Pair</h3>
+<h3 className='py-[2rem] '>Real time Pair</h3>
 
 <div className='grid grid-cols-3 gap-[15px]'>
   <Card/>
